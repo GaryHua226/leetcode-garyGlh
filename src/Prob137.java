c70c01890973e691bf41904b6f069b3e8bdb637b
garyGlh <2366719611@qq.com>
2021-04-30 22:41:22 +0800

    Prob 136 137

---
+++ src/Prob137.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prob137 {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        for (int n : map.keySet()) {
+            if (map.get(n) == 1) {
+                return n;
+            }
+        }
+        return -1;
+    }
+}
