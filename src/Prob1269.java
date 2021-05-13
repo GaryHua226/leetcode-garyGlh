0cb15f26c698c1f45e2752ce644baea2aaf52dc6
garyGlh <2366719611@qq.com>
2021-05-13 09:24:35 +0800

    Prob 1269

---
+++ src/Prob1269.java
@@ -0,0 +1,30 @@
+public class Prob1269 {
+    public int numWays(int steps, int arrLen) {
+        if (arrLen == 1) {
+            return 1;
+        }
+        arrLen = Math.min(steps, arrLen);
+        long[] lastStep = new long[arrLen];
+        lastStep[0] = 1;
+        lastStep[1] = 1;
+        for (int i = 0; i < steps - 1; i++) {
+            long[] nextStep = new long[arrLen];
+            for (int j = 0; j < arrLen; j++) {
+                if (j == 0) {
+                    nextStep[j] = (lastStep[j] + lastStep[j+1]) % (1000000007);
+                } else if (j == arrLen - 1) {
+                    nextStep[j] = (lastStep[j-1] + lastStep[j]) % 1000000007;
+                } else {
+                    nextStep[j] = (lastStep[j-1] + lastStep[j] + lastStep[j+1]) % 1000000007;
+                }
+            }
+            lastStep = nextStep;
+        }
+        System.out.println(lastStep[0]);
+        return (int)lastStep[0];
+    }
+
+    public static void main(String[] args) {
+        new Prob1269().numWays(438,315977);
+    }
+}
